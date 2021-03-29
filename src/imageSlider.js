0e61ed54ecd1c4b26d52e2fa2430efa2bc22e92b
Hoistyr <hoistyd@gmail.com>
2021-03-29 16:32:15 -0600

    Added a timer to the imageSlider

--- src/imageSlider.js
+++ src/imageSlider.js
@@ -1,23 +1,7 @@
-const buildImageSlider = () => {
-  buildNavArrows();
-  buildNavCircles();
-}
-
-const buildNavArrows = () => {
-  console.log('in build nav arrows');
-  const imageSlider = document.getElementById('imageSlider');
-  const rightArrow = document.createElement('img');
-  rightArrow.src = '../src/images/icons/rightArrow.svg';
-  rightArrow.classList.add('rightArrow','sliderArrow');
-  
-  console.log('before left arrow');
-  const leftArrow = document.createElement('img');
-  leftArrow.src = '../src/images/icons/leftArrow.svg';
-  leftArrow.classList.add('leftArrow','sliderArrow');
-  
-  imageSlider.append(rightArrow, leftArrow);
-  rightArrow.addEventListener('click', nextImageRight);
-  leftArrow.addEventListener('click', nextImageLeft);
+const removeAllChildNodes = (parent) => {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
 };
 
 const buildNavCircles = () => {
@@ -25,61 +9,89 @@ const buildNavCircles = () => {
   const sliderImages = document.querySelectorAll('.sliderImage');
   const sliderImagesArray = [...sliderImages];
   removeAllChildNodes(navigationCircles);
-  
-  sliderImagesArray.forEach((image) => { 
-    console.log('in loop');
+
+  sliderImagesArray.forEach((image, index) => {
     const navCircle = document.createElement('div');
     navCircle.classList.add('navCircle');
+    navCircle.dataset.imageid = index;
     navigationCircles.appendChild(navCircle);
-    
+
     if (image.classList.contains('imageVisible')) {
-      console.log('on this image');
       navCircle.classList.add('onImage');
     }
-  })
-}
 
-const nextImageRight = () => {
-  console.log('nextImageRight');
+    if (!image.classList.contains('imageVisible')) {
+      navCircle.addEventListener('click', goToImage);
+    }
+  });
+};
+
+const goToImage = (event) => {
   const sliderImages = document.querySelectorAll('.sliderImage');
   const sliderImagesArray = [...sliderImages];
   const currentIndex = sliderImagesArray.findIndex((image) => image.classList.contains('imageVisible'));
-  const nextIndex = currentIndex + 1;
-  console.log(sliderImagesArray.length);
-  console.log(sliderImagesArray[currentIndex]);
-  console.log(sliderImagesArray[nextIndex]);
-  if (nextIndex < sliderImagesArray.length) {
-    sliderImagesArray[currentIndex].classList.remove('imageVisible');
-    sliderImagesArray[currentIndex].classList.add('imageHidden');
-    sliderImagesArray[nextIndex].classList.remove('imageHidden');
-    sliderImagesArray[nextIndex].classList.add('imageVisible');
-  }
+  const nextIndex = event.target.dataset.imageid;
+  sliderImagesArray[currentIndex].classList.remove('imageVisible');
+  sliderImagesArray[currentIndex].classList.add('imageHidden');
+  sliderImagesArray[nextIndex].classList.remove('imageHidden');
+  sliderImagesArray[nextIndex].classList.add('imageVisible');
 
   buildNavCircles();
-}
+};
+
+const nextImageRight = () => {
+  const sliderImages = document.querySelectorAll('.sliderImage');
+  const sliderImagesArray = [...sliderImages];
+  const currentIndex = sliderImagesArray.findIndex((image) => image.classList.contains('imageVisible'));
+  let nextIndex = currentIndex + 1;
+  if (nextIndex === sliderImagesArray.length) {
+    nextIndex = 0;
+  }
+
+  sliderImagesArray[currentIndex].classList.remove('imageVisible');
+  sliderImagesArray[currentIndex].classList.add('imageHidden');
+  sliderImagesArray[nextIndex].classList.remove('imageHidden');
+  sliderImagesArray[nextIndex].classList.add('imageVisible');
+
+  buildNavCircles();
+};
 
 const nextImageLeft = () => {
   const sliderImages = document.querySelectorAll('.sliderImage');
   const sliderImagesArray = [...sliderImages];
   const currentIndex = sliderImagesArray.findIndex((image) => image.classList.contains('imageVisible'));
-  const nextIndex = currentIndex - 1;
-  console.log(sliderImagesArray.length);
-  console.log(sliderImagesArray[currentIndex]);
-  console.log(sliderImagesArray[nextIndex]);
-  if (nextIndex > -1) {
-    sliderImagesArray[currentIndex].classList.remove('imageVisible');
-    sliderImagesArray[currentIndex].classList.add('imageHidden');
-    sliderImagesArray[nextIndex].classList.remove('imageHidden');
-    sliderImagesArray[nextIndex].classList.add('imageVisible');
+  let nextIndex = currentIndex - 1;
+  if (nextIndex === -1) {
+    nextIndex = sliderImagesArray.length - 1;
   }
-  
+
+  sliderImagesArray[currentIndex].classList.remove('imageVisible');
+  sliderImagesArray[currentIndex].classList.add('imageHidden');
+  sliderImagesArray[nextIndex].classList.remove('imageHidden');
+  sliderImagesArray[nextIndex].classList.add('imageVisible');
+
   buildNavCircles();
-}
+};
 
-const removeAllChildNodes = (parent) => {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  }
-}
+const buildNavArrows = () => {
+  const imageSlider = document.getElementById('imageSlider');
+  const rightArrow = document.createElement('img');
+  rightArrow.src = '../src/images/icons/rightArrow.svg';
+  rightArrow.classList.add('rightArrow', 'sliderArrow');
 
-export default { buildImageSlider }
+  const leftArrow = document.createElement('img');
+  leftArrow.src = '../src/images/icons/leftArrow.svg';
+  leftArrow.classList.add('leftArrow', 'sliderArrow');
+
+  imageSlider.append(rightArrow, leftArrow);
+  rightArrow.addEventListener('click', nextImageRight);
+  leftArrow.addEventListener('click', nextImageLeft);
+};
+
+const buildImageSlider = () => {
+  buildNavArrows();
+  buildNavCircles();
+  setInterval(nextImageRight, 5000);
+};
+
+export default { buildImageSlider };
